d90b2621370c9d8a7e05cd14e5478998299df21a
Skillasaurus <nickfoulksjsu@gmail>
2016-03-14 21:17:14 -0700

    readsync file commit

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var express = require('express')
-var app = express()
 
-/* serves all the static files*/
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
 
-/*serves main page*/
-app.get('/', function(request, response) {
-response.sendfile('index.html') 
-})
+var http = require('http')
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var fs = require('fs')
+
+var call12 = fs.readFileSync('./index.html')
+
+var Server = http.createServer(function(request,response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(call12);
+    response.end();
+});
+
+Server.listen(8081);
+
