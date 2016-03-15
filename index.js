6b734c2fd49b34a1bcfa26f433bc8e18ff2a6610
Skillasaurus <nickfoulksjsu@gmail>
2016-03-14 22:51:08 -0700

    Hey I did read, open, and stat

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var http = require('http')
-var fs = require('fs')
-var Server
-fs.readFile('./index.html', function read(err, data) 
+var http = require('http');
+var fs = require('fs');
+var Server;
+var file = './index.html';
+var buffer = new Buffer(2500);
+fs.open(file, 'r', function(err, fd) 
 {
-    if (err) 
-    {
-        throw err;
-    }
-    content = data;
-    Server = http.createServer(function(request,response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(data);
-    response.end();
-}); 
-Server.listen(8080);               	                // Or put the next step in a function and invoke it
-});
+   	if(err)
+	{
+	    throw " hi";	
+	}	
+    	fs.stat(file, function(err, stats) 
+	{
+       	 fs.read(fd,buffer,0,2500,0,function(err,bytesRead, buffer)
+	  {
+		 if(bytesRead>0)
+		{
+			buffer = buffer.slice(0,bytesRead);
+		}
+		 Server = http.createServer(function(request,response) {
+		 response.writeHead(200, {"Content-Type": "text/html"});
+		 response.write(buffer);
+		 response.end();
+ 		});
+		Server.listen(8080); 
+        });  
+    });
+});              	                
+
 
